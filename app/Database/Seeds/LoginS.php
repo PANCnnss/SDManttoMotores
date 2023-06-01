cb78cd64737c2e0fed582b6dddcbb2ff2177d913
pedroalejandro <pedronuca3@gmail.com>
2023-06-01 10:45:29 -0500

    Filepond y Editar

--- app/Database/Seeds/LoginS.php
+++ app/Database/Seeds/LoginS.php
@@ -22,9 +22,9 @@ class LoginS extends Seeder
 				'NomTUsu'
 			],
 			"data" => [
-				['Técnico'], //1
-				['Supervisor'], //2
-				['Admin'], //3
+				// ['Técnico'], //1
+				// ['Supervisor'], //2
+				// ['Admin'], //3
 			],
 		];
 		$all["menus"] = [
@@ -33,12 +33,14 @@ class LoginS extends Seeder
 			],
 			"data" => [
 				['0','PERSONAL','','ti-id-badge','','1','1'], //1 Personal
-				['1','Diario','D','','dashs/diario','1','1'], //DASH 2,3
-				['1','Mensual','M','','dashs/mensual','1','1'],
-				['0','CRUD','','ti-id-badge','','1','1'], //4 Personal
-				['4','Form Ejemplo','D','','ctrl_fej','1','1'], //CRUD 5
-				['0','PERSONAL','','ti-id-badge','','1','1'], //6 Personal
-				['4','Lista','l','','dashs/diario','1','1'], //CRUD 7
+				['1','Diario','D','','dashs/diario','1','1'], //2 DASH
+				['1','Mensual','M','','dashs/mensual','1','1'], //3
+				['0','PERSONAL','','ti-id-badge','','1','1'], //4 Personal
+				['4','Lista','l','','dashs/diario','1','1'], //5 Dash diario
+				['0','MANTTO MOT','','ti-id-badge','','1','1'], //6 Mantenimiento motores
+				['6','Lista','l','','manttomot','1','1'], //7
+				['0','USUARIOS','','ti-id-badge','','1','1'], //8 Usuarios del sistema
+				['8','Lista','l','','manttomot','1','1'], //9
 			],
 		];
 		$all["menus_tusuarios"] = [
@@ -46,41 +48,44 @@ class LoginS extends Seeder
 				'IdMenu','IdTUsu'
 			],
 			"data" => [
-				// TRAB
-				['4','1',],
-				['5','1',],
-				// JEFE
-				['1','2',],
-				['2','2',],
-				['3','2',],
+				// TEC
+				['6','1',],
+				['7','1',],
+				// SUP
+				['6','2',],
+				['7','2',],
 				// ADMIN
-				['6','3',],
-				['7','3',],
+				['8','3',],
+				['9','3',],
 			],
 		];
-		/*$all["usuarios"] = [
+		$all["usuarios"] = [
 			"camps" => [
 				'NomUsu','LogUsu','ConUsu','IdTUsu'
 			],
 			"data" => [
 				// TRAB
-				['TRAB 1','TRAB1',password_hash("Asdf1234", PASSWORD_DEFAULT),"1"],
-				// JEFE
-				['JEFE 1','JEFE1',password_hash("Asdf1234", PASSWORD_DEFAULT),"2"],
-				// ADMIN
-				['ADMIN 1','ADMIN1',password_hash("Asdf1234", PASSWORD_DEFAULT),"3"],
+				// ['TRAB 1','TRAB1',password_hash("Asdf1234", PASSWORD_DEFAULT),"1"],
+				// // JEFE
+				// ['JEFE 1','JEFE1',password_hash("Asdf1234", PASSWORD_DEFAULT),"2"],
+				// // ADMIN
+				// ['ADMIN 1','ADMIN1',password_hash("Asdf1234", PASSWORD_DEFAULT),"3"],
 				
 			],
-		];*/
+		];
+		echo "-------------SEED----------------\n";
 		foreach ($all as $k => $tab){
 			$camp = $tab['camps'];
+			$nreg = 0;
 			foreach ($tab['data'] as $r){
 				$dt = [];
 				$i = 0;
 				foreach ($r as $c)
 					$dt[$camp[$i ++]] = $c;
 				$this->db->table($k)->insert($dt);
+				$nreg++;
 			}
+			echo "\t Seed $k ($nreg) \n";
 		}
 	}
 }
