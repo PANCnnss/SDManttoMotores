081161df094a63732d6015f3621a765afc22fff4
JasminST <jasalast19@gmail.com>
2023-06-02 09:02:40 -0500

    cambios validacion

--- app/Views/usuarios/lista.php
+++ app/Views/usuarios/lista.php
@@ -30,7 +30,7 @@
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
-      <form id="postUsuario" method="post">
+      <form id="postUsuario" method="post" class="needs-validation form-horizontal">
         <div class="modal-body">
             <div class="row">
                 <div class="col form-group">
@@ -53,7 +53,7 @@
                 </div>
                 <div class="form-group col">
                     <label for="idtusu">Tipo Usuario:</label>
-                    <select id="idtusu" class="form-control">
+                    <select id="idtusu" class="form-control" name="IdTUsu">
                       <option value="1">Técnico</option>
                       <option value="2">Supervisor</option>
                       <option value="3">Admin</option>
@@ -64,7 +64,7 @@
       
         <div class="modal-footer">
             <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
-            <button type="submit" class="btn btn-primary" id="btnSubmit">Guardar</button>
+            <button type="submit" form="postUsuario" class="btn btn-primary" id="btnSubmit">Guardar</button>
         </div>
       </form>
 
@@ -165,6 +165,26 @@
 
 <!-- NUEVO USUARIO Y ACTUALIZACION USUARIO-->
 <script>
+  //Submit form 
+  //  Validacion
+  // (function() {
+  //   'use strict';
+  //   window.addEventListener('load', function() {
+  //     // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  //     var forms = document.getElementsByClassName('needs-validation');
+  //     // Loop over them and prevent submission
+  //     var validation = Array.prototype.filter.call(forms, function(form) {
+  //       form.addEventListener('submit', function(event) {
+  //         if (form.checkValidity() === false) {
+  //           event.preventDefault();
+  //           event.stopPropagation();
+  //         }
+  //         form.classList.add('was-validated');
+  //       }, false);
+  //     });
+  //   }, false);
+  // })();
+
   $(document).ready(function() {
     $('#postUsuario').validate({
       rules: {
